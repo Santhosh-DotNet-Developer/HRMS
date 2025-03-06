2205a4be989ce72233948a6a5cb1c10e4e212f64
Santhosh <santhoshsanthosh8667@gmail.com>
2025-03-06 17:50:25 +0530

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,50 @@
+﻿using HRMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace HRMS
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+
+    public class CheckSessionTimeOutAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var username = filterContext.HttpContext.Request.Cookies[Constant.Username];
+            if (username == null)
+            {
+                filterContext.Result = new RedirectResult("~/Account/SessionOut");
+                if (filterContext.HttpContext.Request.IsAjaxRequest())
+                {
+                    filterContext.HttpContext.Response.StatusCode = 401; // Unauthorized
+                    filterContext.HttpContext.Response.SuppressFormsAuthenticationRedirect = true;
+                    filterContext.Result = new JsonResult
+                    {
+                        Data = new { redirectUrl = "/Account/SessionOut" },
+                        JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                    };
+                }
+                else
+                {
+                    filterContext.Result = new RedirectResult("~/Account/SessionOut");
+                }
+            }
+        }
+    }
+}
